27446c23e1822bdac9c3fbcf07fa893598023a13
Alex Murray <alexmurray@fastmail.fm>
2014-10-14 22:59:11 +1030

    Add .dir-locals.el for emacs flycheck definitions and includes

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((c-mode . ((eval . (let ((includes (split-string (replace-regexp-in-string "-I" "" (shell-command-to-string "pkg-config --cflags-only-I gtk+-3.0 glib-2.0 libpeas-1.0 appindicator3-0.1"))))
+                          (definitions '("HAVE_CONFIG_H")))
+                      (setq includes (append includes (list (concatenate 'string (file-name-directory
+                                                                                  (let ((d (dir-locals-find-file ".")))
+                                                                                    (if (stringp d) d (car d)))) "/indicator-sensors"))))
+                      (setq flycheck-gcc-include-path includes)
+                      (setq flycheck-clang-include-path includes)
+                      (setq flycheck-gcc-definitions definitions)
+                      (setq flycheck-clang-definitions definitions))))))
